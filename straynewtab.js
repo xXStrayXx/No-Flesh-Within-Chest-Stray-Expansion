cd66d96d3444c03712099dd243578d7c3447515b
xXStrayXx <165492914+xXStrayXx@users.noreply.github.com>
2024-04-02 21:17:48 +0800

    Delete straynewtab.js

--- straynewtab.js
+++
@@ -1,3 +0,0 @@
-//const $CreativeTabRegistry = Java.loadClass("dev.architectury.registry.CreativeTabRegistry")
-
-$CreativeTabRegistry.create(Utils.id("kubejs:strayexpansion"), () => Item.of("stray_expansion:frankenstein_heart_active"))
