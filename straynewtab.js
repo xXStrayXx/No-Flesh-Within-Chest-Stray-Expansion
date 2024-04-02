b7a82934e9856292e6ee2bf62cbc4ab5440e1f24
xXStrayXx <165492914+xXStrayXx@users.noreply.github.com>
2024-04-02 21:09:29 +0800

    Add files via upload

---
+++ straynewtab.js
@@ -0,0 +1,3 @@
+//const $CreativeTabRegistry = Java.loadClass("dev.architectury.registry.CreativeTabRegistry")
+
+$CreativeTabRegistry.create(Utils.id("kubejs:strayexpansion"), () => Item.of("stray_expansion:frankenstein_heart_active"))
